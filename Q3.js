0d3b8efbc9cc3333526267b4c245ee3d8b92b90a
Ishit <ishitsingh2001@gmail.com>
2023-01-15 03:04:03 +0530

    feat : add functionality to change the case of all alphabets in a given string

---
+++ Q3.js
@@ -0,0 +1,31 @@
+function changeCase(str){
+    let ans="";
+    for(let ch of str){
+        if(ch>='A' && ch<='Z'){
+            ans+=(ch+"").toLowerCase();
+        }
+        else if(ch>='a' && ch<='z'){
+            ans+=(ch+"").toUpperCase()
+        }
+        else{
+            ans+=ch
+        }
+    }
+    return ans;
+}
+const changeCaseArrow=(str)=>{
+    let ans="";
+    for(let ch of str){
+        if(ch>='A' && ch<='Z'){
+            ans+=(ch+"").toLowerCase();
+        }
+        else if(ch>='a' && ch<='z'){
+            ans+=(ch+"").toUpperCase()
+        }
+        else{
+            ans+=ch
+        }
+    }
+    return ans;
+}
+console.log(changeCaseArrow("aShhQ2zZ"))
