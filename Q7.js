3106c38052c2abedf07ee16a308eb24f8db761ea
Ishit <ishitsingh2001@gmail.com>
2023-01-15 14:41:33 +0530

    feat: add functionality to concatenate 2 strings except their first characters

---
+++ Q7.js
@@ -0,0 +1,7 @@
+function concatenate(stringsArr){
+    return stringsArr[0].substring(1)+stringsArr[1].substring(1);
+}
+const concatenateArow=(stringsArr)=>{
+    return stringsArr[0].substring(1)+stringsArr[1].substring(1);
+}
+console.log(concatenate(["code", "academy"]))
