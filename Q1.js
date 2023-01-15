ea1dfc9c6f00202a7be0941155e5312aa845bb3b
Ishit <ishitsingh2001@gmail.com>
2023-01-15 15:34:28 +0530

    fix(Q1) : replaced if-else statement by ternary operator

--- Q1.js
+++ Q1.js
@@ -1,17 +1,11 @@
 function largestEven(arr){
     const evenArr=arr.filter((n)=>n%2==0);
-    if(evenArr.length==0){
-        return -1;
-    }
-    return Math.max(...evenArr);
+    return (evenArr.length==0?-1:Math.max(...evenArr))
 }
 
 const largestEvenArrow=(arr)=>{
     const evenArr=arr.filter((n)=>n%2==0);
-    if(evenArr.length==0){
-        return -1;
-    }
-    return Math.max(...evenArr);
+    return (evenArr.length==0?-1:Math.max(...evenArr))
 }
 
 console.log(largestEven([1,2,3,4]))
