460558d40f0e7ab5173c50fa88d6d38b1bd9ce40
Ishit <ishitsingh2001@gmail.com>
2023-01-15 14:51:33 +0530

    feat : add functionality to count the number of vowels in a given string

---
+++ Q8.js
@@ -0,0 +1,21 @@
+function countVowels(str){
+    let count=0;
+    const vowels=['a','e','i','o','u','A','E','I','O','U'];
+    for(let ch of str){
+        if(vowels.includes(ch)){
+            count++;
+        }
+    }
+    return count;
+}
+const countVowelsArrow=(str)=>{
+    let count=0;
+    const vowels=['a','e','i','o','u','A','E','I','O','U'];
+    for(let ch of str){
+        if(vowels.includes(ch)){
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countVowelsArrow("codeacademy"))
