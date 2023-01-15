be509f4dd65d70cc90f3e1539b373e5d83c07438
Ishit <ishitsingh2001@gmail.com>
2023-01-15 15:45:56 +0530

    fix(Q6) : remove redundant if-else statements

--- Q6.js
+++ Q6.js
@@ -1,13 +1,7 @@
 function containsScript(str){
-    if(str.length<6){
-        return false;
-    }
     return str.endsWith("Script");
 }
 const containsScriptArrow=(str)=>{
-    if(str.length<6){
-        return false;
-    }
     return str.endsWith("Script");
 }
 console.log(containsScriptArrow("javaScript"))
